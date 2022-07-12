2754342796b883c609ca386e4798a08e4e425672
Poddubniy Sergey <poddubniysergey1981@gmail.com>
2022-07-12 13:03:17 +0300

    IPostController and IPostService interfaces are deleted

--- src/main/java/ru/netology/config/AppConfig.java
+++ src/main/java/ru/netology/config/AppConfig.java
@@ -2,28 +2,26 @@ package ru.netology.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import ru.netology.controller.IPostController;
 import ru.netology.controller.PostController;
 import ru.netology.repository.IPostRepository;
 import ru.netology.repository.PostRepository;
-import ru.netology.service.IPostService;
 import ru.netology.service.PostService;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public IPostController postController (IPostService service) {
+    public PostController postController(PostService service) {
         return PostController.getInstance(service);
     }
 
     @Bean
-    public IPostService postService (IPostRepository repository) {
+    public PostService postService(IPostRepository repository) {
         return PostService.getInstance(repository);
     }
 
     @Bean
-    public IPostRepository postRepository () {
+    public IPostRepository postRepository() {
         return PostRepository.getInstance();
     }
 }
